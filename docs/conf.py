ced1da28e51893152911191f5e0c26a6301e0f3b
ciro spataro <cirospat@gmail.com>
2020-01-21 22:56:40 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ __version__ = '1.0'
 
 # -- General configuration -----------------------------------------------------
 
-source_suffix = '.rst'
+source_suffix = '.rst' 
 master_doc = 'index'
 project = 'Ciro Spataro'
 copyright = '= licenza CC BY Cirospat'
